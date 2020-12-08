b6830aa3a5e042d0ab7407d9fc1e6401f8fca10f
aliimraan <aimraan204@gmail.com>
2020-12-08 16:18:55 +0530

    added index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import {BrowserRouter} from 'react-router-dom'
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <App />hello
   </React.StrictMode>
   </BrowserRouter>
  ,
