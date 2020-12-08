5da2dfa56b40cff3be1b5df099ef33e5e0ac10ec
welcome2rahul <rahul39096@gmail.com>
2020-12-09 03:12:51 +0530

    status

--- frontend/src/components/corporate/Corporate.js
+++ frontend/src/components/corporate/Corporate.js
@@ -1,40 +1,62 @@
 import React from 'react'
 import {Row,Col} from 'react-bootstrap'
+import './corporate.css'
 
 export default function Corporate() {
     return (
-        <div className="main">
-        <Row>
-            <Col md={2}></Col>
-            <Col md={8}>
-            <form>
-            <h3 className="text-center">Sign In</h3>
-    
-            <div className="form-group">
-                <label>Email address</label>
-                <input type="email" className="form-control" placeholder="Enter email" />
-            </div>
-    
-            <div className="form-group">
-                <label>Password</label>
-                <input type="password" className="form-control" placeholder="Enter password" />
-            </div>
-    
-            <div className="form-group">
-                <div className="custom-control custom-checkbox">
-                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                    <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                </div>
-            </div>
-    
-            <button type="submit" className="btn btn-primary btn-block">Submit</button>
-            <p className="forgot-password text-right">
-                Forgot <a href="#">password?</a>
-            </p>
-        </form>
-            </Col>
-            <Col md={2}></Col>
-        </Row>
-        </div>
+        <div className="limiter">
+		<div className="container-login100">
+			<div className="wrap-login100">
+            
+				<form className="login100-form validate-form">
+                
+                
+                    
+					
+
+					<span className="login100-form-title p-b-43">
+						Login to continue
+					</span>
+					
+                    
+					
+					<div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input className="input100" type="text" name="email" />
+						<span className="focus-input100"></span>
+						<span className="label-input100">Email</span>
+					</div>
+					
+                   
+					
+					<div className="wrap-input100 validate-input" data-validate="Password is required">
+						<input className="input100" type="password" name="pass" />
+						<span className="focus-input100"></span>
+						<span className="label-input100">Password</span>
+					</div>
+
+					<div className="flex-sb-m w-full p-t-3 p-b-32">
+                    <input className="input-checkbox100" id="ckb1" type="checkbox" name="remember-me" />
+							<label className="label-checkbox100" for="ckb1">
+								Login As Admin
+							</label>
+
+					
+					</div>
+			
+
+					<div className="container-login100-form-btn">
+						<button className="login100-form-btn">
+							Login
+						</button>
+					</div>
+					
+					
+					
+				</form>
+
+				</div>
+			</div>
+		</div>
+	
     )
 }
