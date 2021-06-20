7b85e76b994e2a3cc96ba4f4645048a8d6e2e3ad
aliimraan <aimraan204@gmail.com>
2021-06-20 22:04:09 +0530

    remove

--- server.js
+++ server.js
@@ -24,9 +24,6 @@ const PORT=process.env.PORT || 5000
 if(process.env.NODE_ENV="production"){
     app.use(express.static('client/build'))
     
-    app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
 }
 
 
