85abfc810699e5b693d4bce7d2fc44e3c563848e
aliimraan <aimraan204@gmail.com>
2021-06-20 21:03:39 +0530

    ready to deploy

---
+++ server.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const app=express();
+const cors=require('cors')
+const body_parser=require('body-parser')
+require('dotenv').config();
+const userRouter=require('./routes/user')
+const applicationRouter=require('./routes/application')
+
+
+mongoose.connect(process.env.DB_CONN,{ useNewUrlParser: true ,useUnifiedTopology: true}).then(()=>{
+    console.log('db connected')
+}).catch((err)=>{
+    console.log(err)
+})
+
+app.use(cors())
+app.use(body_parser.json())
+app.use('/api',userRouter)
+app.use('/application',applicationRouter)
+
+const PORT=process.env.PORT || 5000
+
+if(process.env.NODE_ENV="production"){
+    app.use(express.static('client/build'))
+}
+
+
+app.listen(PORT,()=>{
+    console.log('server started')
+})
